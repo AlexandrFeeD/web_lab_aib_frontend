34811967e2cb197d126eb443dd0a2573ec1fd974
AlexandrFeeD <sanka.tsoi.2003@gmail.com>
2023-12-24 22:37:33 +0300

    Lab13 fixed

--- labs/Lab_13_python_oop/solution/main.py
+++ labs/Lab_13_python_oop/solution/main.py
@@ -3,16 +3,14 @@ import json
 import os
 from writer import Writer
 
-BASE_PATH = r"C:\Users\Я\Desktop\Коды\WEB\web_lab_aib_frontend\labs\Lab_13_python_oop"
-
 def load_data(file_path):
-    full_path = os.path.join(BASE_PATH, file_path)
+    full_path = os.path.join(os.getcwd(), file_path)
     with open(full_path, 'r', encoding='utf-8') as file:
         return json.load(file)
 
 def generate_report_filename():
     timestamp = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
-    return os.path.join(BASE_PATH, f'my_payments_analytics_{timestamp}.xlsx')
+    return os.path.join(os.getcwd(), f'my_payments_analytics_{timestamp}.xlsx')
 
 def main():
     clients_data = load_data('clients.json')['clients']
